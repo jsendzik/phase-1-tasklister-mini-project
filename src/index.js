1c2025d99083627377e8db3692c3ee3ea9d8a123
Jacob Sendzik <jake.sendzik@gmail.com>
2023-09-09 11:01:20 -0400

    Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let inp = document.getElementById("new-task-description")
+    handleToDo(inp.value)
+    form.reset()
+  })
+
+
+
 });
+function handleToDo(todo) {
+  let p = document.createElement("p")
+  let btn = document.createElement("button")
+  btn.textContent = "X"
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  document.querySelector("#list").appendChild(p)
+  btn.addEventListener("click", () => {p.remove()})
+}
